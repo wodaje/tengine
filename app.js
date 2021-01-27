813440927a9e7a34d9cc80033aa8bac8c4ee572f
Jeff Woda <wodahouse@gmail.com>
2021-01-27 09:35:47 -0500

    finished inquirer prompt data adding

--- app.js
+++ app.js
@@ -4,16 +4,158 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+// const { } = require("os");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+// const OUTPUT_DIR = path.resolve(__dirname, "output");
+// const outputPath = path.join(OUTPUT_DIR, "main.html");
 
-const render = require("./lib/htmlRenderer");
+// const render = require("./lib/htmlRenderer");
+
+const empArray = ['Engineer', 'Manager', 'Intern']
+
+let employees = [];
+
+ const addEm = async () => {
+
+  let buttocks = await inquirer.prompt([ 
+    {
+        type:"input",
+        name: "name",
+        message: "Please input Employee Name"
+        },
+    {
+        type:"number",
+        name: "id",
+        message: "Enter Employee Id number"
+        },
+    {
+        type:"input",
+        name: "email",
+        message: `Enter email`
+        },
+    {
+        type:"list",
+        name: "type",
+        message: "Choose Employee Type",
+        choices: empArray
+     }
+     
+
+])
+return buttocks
+      //console.log(out.type) 
+    //  switch(out.type){
+    //      case "Manager":
+    //      console.log("manager selected");
+    //  }
+     
+
+  
+}
+  // inquirer
+  // .prompt(quest)
+  // .then((ans) => {
+    
+  //   switch (ans.title) {
+    
+  //     case "Manager":
+  //       employees.push(new Manager(ans.name, ans.id, ans.email, ans.officeNumber))
+  //       break;
+    
+  //     case "Engineer":
+  //       employees.push(new Engineer(ans.name, ans.id, ans.email, ans.github))
+  //       break;
+    
+  //     case "Intern":
+  //       employees.push(new Intern(ans.name, ans.id, ans.email, ans.school))
+  //       break;
+  //   }
+
+    
+  //   if (ans.askAgain === true) {
+  //     addEm()
+  //   } else {
+  //     renderOut()
+  //   }
+  // })
 
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+  async function nextAns(out){
+    console.log(out.type)  
+            
+    switch (out.type) {
+    
+      case "Manager":
+      inquirer.prompt  
+        var outc = await inquirer.prompt([
+        {
+            type:"input",
+            name: "officeNumber",
+            message: "Office Number"
+        }])
+        employees.push(new Manager(out.name, out.id, out.email, outc.officeNumber))
+        break;
+    
+      case "Engineer":
+        var outc = await inquirer.prompt([
+        {
+            type:"input",
+            name: "github",
+            message: "Git Hub"
+        }])  
+      employees.push(new Engineer(out.name, out.id, out.email, outc.github))
+        break;
+    
+      case "Intern":
+        var outc = await inquirer.prompt([
+        {
+            type:"input",
+            name: "school",
+            message: "School"
+        }])
+        employees.push(new Intern(out.name, out.id, out.email, outc.school))
+        break;
+    }
 
+
+  }
+
+async function  looper(){
+    var ans = await inquirer.prompt([
+        {
+            type:"confirm",
+            name: "answer",
+            message: "Do you want to add another employee"
+        }])
+
+    if (ans.answer === true) {
+           await  init()
+    } else {   
+        renderOutput()
+    }
+}
+
+
+function renderOutput(){
+console.log(employees)
+
+}
+
+const init =  async () => {
+    try {
+        
+        const out = await addEm()
+        await nextAns(out)
+        await looper()
+        // const Html = genHtml(out)
+        // await writeFsA("README.md", MD)
+        // console.log("Successfully generated Web Page")     
+    }catch(error){
+        console.log(error)
+    }
+}
+
+init()
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
